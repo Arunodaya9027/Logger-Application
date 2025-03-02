d6b222c75f2565602dc91ba81d1afeb409d301c9
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-03-02 16:20:47 +0530

    [Arunodaya Pratap Singh] add. Created a Demo Bean

--- src/main/java/com/example/loggerapplication/LoggerApplication.java
+++ src/main/java/com/example/loggerapplication/LoggerApplication.java
@@ -2,12 +2,14 @@ package com.example.loggerapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.example.loggerapplication.component.DemoBean;
 
 @SpringBootApplication
 public class LoggerApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(LoggerApplication.class, args);
+        System.out.println("Welcome to Spring Boot");
+        ApplicationContext context = SpringApplication.run(LoggerApplication.class, args);
+        System.out.println("Context: " + context.getBean(DemoBean.class));
     }
-
 }
